319a36faecd0ec31c31ef528d81cfac1731c28a7
Oscar Garcia <hello@ogarcia.io>
2025-01-23 23:49:31 -0800

    Get sprites for pokemon

--- scripts/make-data.ts
+++ scripts/make-data.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
-import { writeFileSync } from "node:fs";
+import { existsSync, writeFileSync } from "node:fs";
 
-const ENDPOINT = "https://pokeapi.co/api/v2/pokemon/?limit=149";
+const LIMIT = 149;
+const ENDPOINT = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 const dataSchema = z.object({
   results: z.array(
@@ -12,22 +13,42 @@ const dataSchema = z.object({
   ),
 });
 
+const getSpriteUrl = (id: number) =>
+  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/${id}.png`;
+
 async function main() {
   const response = await fetch(ENDPOINT);
   if (!response.ok) {
     throw new Error(`HTTP error: ${response.status}`);
   }
   const data = dataSchema.parse(await response.json()).results;
-  const mapped = data.map((e) => {
+  const pokemons = data.map((e) => {
     const splitUrl = e.url.split("/");
-    const id = splitUrl[splitUrl.length - 2];
+    const id = Number(splitUrl[splitUrl.length - 2]);
     return {
       ...e,
       id,
     };
   });
-  console.log(mapped);
-  writeFileSync("src/assets/data.json", JSON.stringify(mapped, null, 2));
+  writeFileSync("src/assets/data.json", JSON.stringify(pokemons, null, 2));
+
+  // fetch sprites
+  for (const pokemon of pokemons) {
+    const filename = `src/assets/sprites/${pokemon.id}.png`;
+    if (existsSync(filename)) {
+      continue;
+    }
+    const spriteUrl = getSpriteUrl(pokemon.id);
+    const response = await fetch(spriteUrl);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+
+    const blob = await response.blob();
+    const buffer = Buffer.from(await blob.arrayBuffer());
+    writeFileSync(filename, buffer);
+  }
 }
 
 try {
